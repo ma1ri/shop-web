464b45108739e7e6c3335f0b8df938b82919c5e6
ma1ri <maval15@freeuni.edu.ge>
2024-06-02 08:52:06 +0400

    feat: added body-parser and dummy code to test it

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,44 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const app = express();
 
 app.get("/favicon.ico", (req, res) => res.status(204).end());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  console.log(`Request URL: ${req.url}`);
-  console.log("firsts middleware");
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, PUT, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send("hello from express");
+app.post("/api/posts/", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "created",
+  });
+});
+
+app.use("/api/posts/", (req, res, next) => {
+  const bla = [
+    {
+      id: "alskjddaslkjd",
+      value: "aklsjdajdalksjdslkajsdlkajsdlkj",
+    },
+  ];
+  res.json({
+    message: "success",
+    bla,
+  });
 });
 
 module.exports = app;
